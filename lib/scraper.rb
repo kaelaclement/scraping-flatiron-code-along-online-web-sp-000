e9127ebae36fa5c05347403467bf3a00cfd7bc59
Kaela Clement <stephanie.kaela@gmail.com>
2018-11-05 04:33:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,12 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
+  end
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +20,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
